ddb426f336006eb8bc75bd833303a5751eb78b11
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-11-29 14:41:29 +0530

    completed tic tac toe game

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -9,7 +9,7 @@ def toss_coin():
 
 def user_coin_choices(tossed_value):
     print("Choose between heads or tails")
-    player_1 = input("player one Enter your prediction: ")
+    player_1 = input("Player one, enter your prediction: ").lower()
     if tossed_value == player_1:
         print("Player one won the toss")
         return "player_one"
@@ -19,13 +19,68 @@ def user_coin_choices(tossed_value):
 
 
 def choose_x_or_o(chosen_player):
-    player1_choice = input(f"{chosen_player} choose first between x or o: ")
-    player2_choice = ""
-    if player1_choice == "x":
-        player2_choice = "o"
-    else:
-        player2_choice = "x"
-    return [player1_choice, player2_choice]
+    while True:
+        player1_choice = input(f"{chosen_player}, choose X or O: ").upper()
+        if player1_choice in ["X", "O"]:
+            player2_choice = "O" if player1_choice == "X" else "X"
+            return [player1_choice, player2_choice]
+        else:
+            print("Invalid choice. Please choose X or O.")
+
+
+def print_board(game_matrix):
+    for row in game_matrix:
+        print(" ".join(row))
+    print()
+
+
+def check_win_condition(game_matrix):
+    # Check rows, columns, and diagonals
+    for i in range(3):
+        if (game_matrix[i][0] == game_matrix[i][1] == game_matrix[i][2] != "_") or \
+                (game_matrix[0][i] == game_matrix[1][i] == game_matrix[2][i] != "_"):
+            return True
+    if (game_matrix[0][0] == game_matrix[1][1] == game_matrix[2][2] != "_") or \
+            (game_matrix[0][2] == game_matrix[1][1] == game_matrix[2][0] != "_"):
+        return True
+    return False
+
+
+def is_board_full(game_matrix):
+    return all(cell != "_" for row in game_matrix for cell in row)
+
+
+def play_turn(game_board, player_turn):
+    while True:
+        print_board(game_board)
+        coordinates = input(f"{player_turn}, enter row and column (e.g., 0 1): ").split()
+
+        if len(coordinates) != 2:
+            print("Invalid input. Please enter row and column separated by a space.")
+            continue
+
+        row_coordinate, col_coordinate = map(int, coordinates)
+
+        if not (0 <= row_coordinate < 3 and 0 <= col_coordinate < 3):
+            print("Invalid coordinates. Row and column must be between 0 and 2.")
+            continue
+
+        if game_board[row_coordinate][col_coordinate] != "_":
+            print("The position is already taken. Choose another position.")
+        else:
+            game_board[row_coordinate][col_coordinate] = player_turn
+            break
+
+    if check_win_condition(game_board):
+        print_board(game_board)
+        print(f"{player_turn} wins!")
+        return True
+    elif is_board_full(game_board):
+        print_board(game_board)
+        print("It's a tie!")
+        return True
+
+    return False
 
 
 def create_matrix(length):
@@ -35,37 +90,16 @@ def create_matrix(length):
     return parent_list
 
 
-def play_turn(game_board, player_turn):
-    updated_game_board = game_board
-    correct_condition = False
-
-    while not correct_condition:
-        coordinates = input(f"{player_turn} Enter coordinates with spaces: ").split(" ")
-        row_coordinate = int(coordinates[0])
-        column_coordinate = int(coordinates[1])
-
-        if updated_game_board[row_coordinate][column_coordinate] != "_":
-            print("The position is already taken, choose another position")
-        else:
-            updated_game_board[row_coordinate][column_coordinate] = player_turn
-            print(updated_game_board)
-            correct_condition = True
-
-    return updated_game_board
-
-
 def main():
     game_over = False
     turn = 0
     game_board = create_matrix(3)
     chosen_turns = choose_x_or_o(user_coin_choices(toss_coin()))
+
     while not game_over:
-        game_board = play_turn(game_board, chosen_turns[turn])
-        if not play_turn(game_board, chosen_turns[turn]):
-            turn = turn
-        else:
-            turn = (turn + 1) % 2
+        game_over = play_turn(game_board, chosen_turns[turn])
+        turn = (turn + 1) % 2
 
 
-
-main()
+if __name__ == "__main__":
+    main()
