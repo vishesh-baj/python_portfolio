f3672f1ae85fe1e3f9c08cd9a507a5c1230c99fe
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-12-05 18:40:11 +0530

    added turtle script

--- turtle_lib/main.py
+++ turtle_lib/main.py
@@ -1,11 +1,44 @@
+import turtle
 from turtle import Turtle, Screen
-import prettytable
+from random import choice
+timmy_the_turtle = Turtle()
 
-# timmy = Turtle()
-# my_screen = Screen()
-# timmy.shape("turtle")
-# timmy.color("red")
-#
-#
-# my_screen.exitonclick()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("teal")
 
+
+def draw_square():
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+    turtle.dot()
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+
+
+def draw_dashed_line():
+    for i in range(0, 15):
+        timmy_the_turtle.forward(10)
+        timmy_the_turtle.penup()
+        timmy_the_turtle.forward(10)
+        timmy_the_turtle.pendown()
+
+
+colors = ["red", "blue", "black", "teal", "pink", "gray", "black"]
+
+
+def draw_shapes():
+    for i in range(3, 11):
+        angle = 360 / i
+        for j in range(0, i):
+            timmy_the_turtle.forward(100)
+            timmy_the_turtle.right(angle)
+        timmy_the_turtle.color(choice(colors))
+
+
+draw_shapes()
+screen = Screen()
+screen.exitonclick()
